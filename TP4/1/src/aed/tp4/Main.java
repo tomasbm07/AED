358bba456ad4995700dcd299fc262234de07b616
Tomás Mendes <43079732+tomasbm07@users.noreply.github.com>
2021-04-19 17:25:38 +0000

    feito

--- TP4/1/src/aed/tp4/Main.java
+++ TP4/1/src/aed/tp4/Main.java
@@ -8,33 +8,40 @@ import java.util.Random;
 import java.io.*;
 
 public class Main {
-    private static final int SIZE = 1000000;
+    private static final int SIZE = 10000000;
 
     public static void main(String[] args) {
         List<Integer> N = new ArrayList<>();
-        List<Long> tempos = new ArrayList<>();
+        List<Float> tempos = new ArrayList<>();
 
         ShellSort SS = new ShellSort();
         generateInput generator = new generateInput();
 
-        long tempo;
+        float tempo;
 
-        for (int i = (int) (SIZE * 0.1); i < SIZE; i += (int) (SIZE * 0.1)) {
+        for (int i = (int) (SIZE * 0.05); i <= SIZE; i += (int) (SIZE * 0.05)) {
             SS.setArray(generator.random(i));
             //SS.setArray(generator.decrescente(i));
             //SS.setArray(generator.ordenado5Random(i));
             //SS.setArray(generator.ordenado1Random(i));
+
             tempo = SS.base_sort();
+            //tempo = SS.improved_sort_1();
+            //tempo = SS.improved_sort_2();
+            //tempo = SS.improved_sort_3();
+            //tempo = SS.improved_sort_4();
+
+            tempo = tempo / 1000;
             N.add(i);
             tempos.add(tempo);
-            System.out.format("N = %d, T = %d ms\n", i, tempo);
+            System.out.format("N = %d, T = %.3f s\n", i, tempo);
         }
 
         try {
             FileWriter f = new FileWriter("results.csv");
             f.write("N,tempo(ms)\n");
             for (int i = 0; i < N.size(); i++) {
-                f.write("%d,%d\n".formatted(N.get(i), tempos.get(i)));
+                f.write("%d,%.3f\n".formatted(N.get(i), tempos.get(i)));
             }
             f.close();
             Desktop.getDesktop().open(new File("results.csv"));
@@ -81,7 +88,7 @@ class generateInput {
         }
 
         for (int i = 0; i < (int) (size * 0.05); i++) {
-            array.add(rand.nextInt(this.max_int));
+            array.set(this.array.size(), this.max_int);
         }
 
         return array;
@@ -95,7 +102,7 @@ class generateInput {
         }
 
         for (int i = 0; i < (int) (size * 0.01); i++) {
-            array.add(rand.nextInt(this.max_int));
+            array.set(this.array.size(), this.max_int);
         }
 
         return array;
--- TP4/1/src/aed/tp4/shellSort.java
+++ TP4/1/src/aed/tp4/shellSort.java
@@ -1,13 +1,15 @@
 package aed.tp4;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 class ShellSort {
     ArrayList<Integer> array;
 
-    public ShellSort() {}
+    public ShellSort() {
+    }
 
     public ShellSort(ArrayList<Integer> array) {
         this.array = array;
@@ -17,7 +19,7 @@ class ShellSort {
         this.array = array;
     }
 
-    private void shellSort(List<Integer> gaps){
+    private void shellSort(List<Integer> gaps) {
         int temp;
         int i, j;
 
@@ -37,25 +39,26 @@ class ShellSort {
 
         //create gaps
         List<Integer> gaps = new ArrayList<>();
-        for (int i = 0; i < 15; i++) {
+        for (int i = (int) (Math.log(this.array.size()) / Math.log(2)); i >= 0; i--) {
             gaps.add((int) Math.pow(2, i));
         }
-
+        //System.out.println(gaps);
         long start_time = System.nanoTime();
 
         //shell sort algorithm
         shellSort(gaps);
 
         final_time = System.nanoTime() - start_time;
+        //System.out.println(array);
         return final_time / 1000000; //ms
     }
 
-    public long improved_sort_1() {
+    public float improved_sort_1() {
         long final_time;
 
         //create gaps
         List<Integer> gaps = new ArrayList<>();
-        for (int i = this.array.size()/2; i > 0; i /= 2) {
+        for (int i = (int) (this.array.size() / 2.2); i > 0; i /= 2) {
             gaps.add(i);
         }
 
@@ -64,7 +67,79 @@ class ShellSort {
         shellSort(gaps);
 
         final_time = System.nanoTime() - start_time;
-        return final_time / 1000000; //ms
+        return (float) (final_time / 1000000); //ms
+    }
+
+    //lista de gaps: https://oeis.org/A033622
+    public float improved_sort_2() {
+        long final_time;
+
+        int[] aux = {1, 5, 19, 41, 109, 209, 505, 929, 2161, 3905, 8929, 16001,
+                36289, 64769, 146305, 260609, 587521, 1045505, 2354689,
+                4188161, 9427969, 16764929, 37730305, 67084289,
+                150958081, 268386305, 603906049, 1073643521};
+
+        List<Integer> gaps = new ArrayList<>();
+        for (int x : aux) {
+            gaps.add(x);
+        }
+        Collections.reverse(gaps);
+
+        long start_time = System.nanoTime();
+
+        shellSort(gaps);
+
+        final_time = System.nanoTime() - start_time;
+        return (float) (final_time / 1000000); //ms
+    }
+
+    //lista de gaps: https://oeis.org/A108870 - Tokuda's gaps
+    public float improved_sort_3() {
+        long final_time;
+
+        //create gaps
+        int[] aux = {1, 4, 9, 20, 46, 103, 233, 525, 1182, 2660, 5985, 13467,
+                30301, 68178, 153401, 345152, 776591, 1747331, 3931496,
+                8845866, 19903198, 44782196, 100759940, 226709866,
+                510097200, 1147718700};
+
+        List<Integer> gaps = new ArrayList<>();
+        for (int x : aux) {
+            gaps.add(x);
+        }
+        Collections.reverse(gaps);
+
+        long start_time = System.nanoTime();
+
+        shellSort(gaps);
+
+        final_time = System.nanoTime() - start_time;
+        return (float) (final_time / 1000000); //ms
+    }
+
+    //lista de gaps: https://oeis.org/A000225
+    public float improved_sort_4() {
+        long final_time;
+
+        //create gaps
+        int[] aux = {1, 3, 7, 15, 31, 63, 127, 255, 511, 1023, 2047, 4095, 8191,
+                16383, 32767, 65535, 131071, 262143, 524287, 1048575,
+                2097151, 4194303, 8388607, 16777215, 33554431,
+                67108863, 134217727, 268435455, 536870911, 1073741823,
+                2147483647};
+
+        List<Integer> gaps = new ArrayList<>();
+        for (int x : aux) {
+            gaps.add(x);
+        }
+        Collections.reverse(gaps);
+
+        long start_time = System.nanoTime();
+
+        shellSort(gaps);
+
+        final_time = System.nanoTime() - start_time;
+        return (float) (final_time / 1000000); //ms
     }
 
 }
